8d4000649ceaf7f6d138eb462fb96bb05666ca93
Mitaanshu <mitaanshu137@gmail.com>
2019-03-11 04:00:35 +1100

    refactor

--- index.js
+++ index.js
@@ -5,5 +5,5 @@
  app.use(express.json())
  app.use('/api/worker/jobs',workers)
 
- const port = process.env.PORT || 5000
+ const port = process.env.PORT || 6000
  app.listen(port, () => console.log(`Listenning on port ${port}`))
